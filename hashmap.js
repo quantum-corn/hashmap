462e9acd6528b6ecbe0e78d641d3201c0643dac7
quantum-corn <akashkhetan044@gmail.com>
2024-12-12 17:15:09 +0530

    Initialized an npm package
    Separate the testing code into another file

--- hashmap.js
+++ hashmap.js
@@ -1,17 +1,38 @@
+import LinkedList from "./linked-list.js";
+
 const HashMap = function (capacity, loadFactor) {
+  let hashMap = [];
+
   const hash = function (key) {
     const primeFactor = 31;
     let hashCode = 0;
-    for (i = 0; i < key.length; i++) {
+    for (let i = 0; i < key.length; i++) {
       hashCode = (hashCode * primeFactor + key.charCodeAt(i)) % capacity;
     }
     return hashCode;
   };
 
+  const validateIndex = function (index) {
+    if (index < 0 || index >= hashMap.length)
+      throw new Error("Error: Index Out of Bound");
+  };
+
+  const set = function (key, value) {
+    let hashCode = hash(key);
+    validateIndex(hashCode);
+    console.log('setting the provided data');
+  }
+
+  const get = function (key) {
+    let hashCode = hash(key);
+    validateIndex(hashCode);
+    console.log('getting the requested data');
+  };
+
   return {
-    hash,
+    set,
+    get,
   };
 };
 
-const test = HashMap(16, 0.8);
-console.log(test.hash('Ash'));
+export default HashMap;
---
+++ test.js
@@ -0,0 +1,3 @@
+import HashMap from './hashmap.js'
+
+const test = HashMap(16, 0.75);
